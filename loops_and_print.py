f52eaae192ea23a1cdbc51fa78662c0d6af068ea
valentinaotero <162721713+valentinaotero@users.noreply.github.com>
2024-05-17 12:28:41 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -8,7 +8,17 @@ def enumerate_list(list):
     return result
 
 
-print(enumerate_list(["Red", "Green", "", "White", "Black"]))
+
+
+def enumerate_backwards(list):
+    result = []
+    index = 0
+    for string in list:
+        if string:
+            result.append (f"{index}. {string[ : :-1]}")
+            index +=1
+    return result
+
 
 
 
